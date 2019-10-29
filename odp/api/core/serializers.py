f95e3f47be27fdac637a9d02742d59fee5a1920d
mmartyn <maxmartnyak@gmail.com>
2019-10-29 11:54:20 +0300

    fixed create

--- odp/api/core/serializers.py
+++ odp/api/core/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from odp.apps.core.models import Team
-from .wrappers import OpenDotaClient
 
 
 class TeamSerializer(serializers.ModelSerializer):
@@ -14,21 +13,3 @@ class TeamCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = ('name',)
-
-    def create(self, validated_data):
-        name = validated_data.get('name')
-        client = OpenDotaClient()
-        team_json = client.get_team_by_name(name)
-        if team_json is not None:
-            team = Team.objects.create(
-                team_id=team_json['team_id'],
-                rating=team_json['rating'],
-                wins=team_json['wins'],
-                losses=team_json['losses'],
-                last_match_time=team_json['last_match_time'],
-                name=team_json['name'],
-                tag=team_json['tag'],
-                logo_url=team_json['logo_url'],
-            )
-            return team
-        raise serializers.ValidationError("Team with this name can't be found")
--- odp/api/core/views.py
+++ odp/api/core/views.py
@@ -1,14 +1,25 @@
-from .serializers import TeamSerializer, TeamCreateSerializer
+from .serializers import TeamSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.response import Response
 from odp.apps.core.models import Team
+from .wrappers import OpenDotaClient
+from rest_framework import status
+from rest_framework import serializers
 
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     permission_classes = (IsAuthenticated,)
+    serializer_class = TeamSerializer
 
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return TeamCreateSerializer
-        return TeamSerializer
+    def create(self, request, *args, **kwargs):
+        name = request.data['name']
+        client = OpenDotaClient()
+        team_json = client.get_team_by_name(name)
+        if team_json is not None:
+            serializer = self.get_serializer(data=team_json)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        raise serializers.ValidationError("Team with this name can't be found")
