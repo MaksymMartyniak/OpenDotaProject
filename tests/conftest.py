9c3035b507d47e65fb91cec5981cf86335718339
mmartyn <maxmartnyak@gmail.com>
2019-10-31 16:58:49 +0300

    get teams list test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from odp.apps.core.models import Team
 
 
 @pytest.fixture()
@@ -18,3 +19,10 @@ def api_client_auth_user(create_user):
     user = User.objects.get(username="TestUser")
     client.force_authenticate(user=user, token="Token 12345")
     return client
+
+
+@pytest.fixture()
+def create_team(db):
+    return Team.objects.create(team_id=1111, rating=1.1, wins=2, losses=2,
+                               last_match_time=1234, name="TestTeam", tag="TT",
+                               logo_url="https://fake.png")
