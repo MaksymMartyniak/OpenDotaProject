70fcbf2f9d0e4be9223b645a098208c07d699be9
mmartyn <maxmartnyak@gmail.com>
2019-11-01 12:56:09 +0300

    pep corections

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -6,9 +6,10 @@ from rest_framework.test import APIClient
 def test_authentication(create_user):
     client = APIClient()
 
-    response = client.post('/api/obtain-token/',
-                           {'username': 'TestUser', 'password': '12345'}
-                           )
+    response = client.post(
+        '/api/obtain-token/',
+        {'username': 'TestUser', 'password': '12345'}
+    )
 
     assert len(response.json()['token']) == 40
     assert response.status_code == 200
@@ -17,9 +18,10 @@ def test_authentication(create_user):
 @pytest.mark.django_db
 def test_no_authentication():
     client = APIClient()
-    response = client.post('/api/obtain-token/',
-                           {'username': 'NoTestUser', 'password': '1111'}
-                           )
+    response = client.post(
+        '/api/obtain-token/',
+        {'username': 'NoTestUser', 'password': '1111'}
+    )
 
     assert response.status_code == 400
     assert response.json()["non_field_errors"] == [
